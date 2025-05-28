4917bd65d62aed3d1f6705cd7894b68b20f1947b
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-05-28 13:50:55 +0500

    full

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ width: "100dvh", height: "100dvh" }}>
+      <iframe src="../public/Test_OT.txt" width="100%" height="100%"></iframe>
+    </div>
+  );
+}
+
+export default App;
