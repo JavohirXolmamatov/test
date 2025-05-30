6ed2b58f7e3ba914c76ec937dd9346940757922d
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-05-30 16:45:34 +0500

    intelectual

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div style={{ width: "100dvh", height: "100dvh" }}>
-      <h1 style={{ textAlign: "center" }}>Test</h1>
-      <iframe src="/Test_OT.txt" width="100%" height="100%"></iframe>
+    <div className="container">
+      <h1 className="title">Test</h1>
+      <iframe
+        src="/Malumotlar intellektual tahlili 2025.txt"
+        title="Text File"
+        className="responsive-iframe"
+      ></iframe>
     </div>
   );
 }
