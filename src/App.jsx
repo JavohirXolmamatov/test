1bc7c546617d219c0c0aab70c8a0dd0608fb73e5
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-05-31 12:59:02 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Hammamiz kirib ketaylik</h1>
+      <h1 className="title">Hammamiz kirib ketaylik🤣</h1>
       <iframe
         src="/Test_OT.txt"
         title="Text File"
